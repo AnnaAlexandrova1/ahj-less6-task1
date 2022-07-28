c454b81feef795eaf8cb12ef3f0203cffccdf636
Anna <anna4776497@gmail.com>
2022-07-28 09:52:12 +0300

    верстка завершена

---
+++ webpack.common.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	target: 'web',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+      },
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+          },
+        ],
+      },
+			{
+				test: /\.(?:ico|png|jpg|jpeg|gif)$/i,
+					
+				type:'asset/resource',
+					
+      },
+			{
+				test: /\.css$/,
+				use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+	  {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+			{
+				test: /\.svg$/,
+				type: 'asset/resource',
+      },
+    ],
+	},
+	plugins: [
+    new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+    new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+		}),
+  ],
+};
