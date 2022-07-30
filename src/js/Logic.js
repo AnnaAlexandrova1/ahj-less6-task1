0c2f470ed64cf45b04b2b30ec53ea6a57a945d19
Anna <anna4776497@gmail.com>
2022-07-30 12:12:04 +0300

    Добавляется форма

--- src/js/Logic.js
+++ src/js/Logic.js
@@ -8,5 +8,31 @@ export default class Logic {
 
   init() {
     this.board.getBoard();
+    this.container = document.querySelector('.container');
+    this.onClickAddCard();
+  }
+
+  static get creatingCard() {
+    return `
+    <div class="form">
+    <input class="textarea" placeholder='Enter your task here'>
+    </input>
+    <div class="buttons">
+        <button class="add_item-btn">Add</button>
+        <button class="cancel_item-btn">Cancel</button>
+    </div>
+</div>`;
+  }
+
+  onClickAddCard() {
+    this.addCardBtn = document.querySelectorAll('.add_btn');
+    this.addCardBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        console.log(elem.parentElement);
+        elem.parentElement.classList.add('item_active');
+        elem.parentElement.insertAdjacentHTML('beforeend', this.constructor.creatingCard);
+        elem.classList.add('hidden');
+      });
+    });
   }
 }
